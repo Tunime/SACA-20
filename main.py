e7f88994d847a26e30e9dcd50b8083105978ce2e
AlvaroMoCa <almaseb_123@hotmail.com>
2017-06-16 22:08:30 -0500

    Incorporando la primera prate

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import time
+
+hora=time.strftime("%I:%M")
+computer="#Sheldon > "
+user="#Alvaro  > "
+print("")
+print("")
+print(computer,"Hola son las ",hora)
+print(computer,"Quieres jugar (si/no)")
+respuesta=str(input(user))
+
+
+#abrir un web
+#os.system("start https://trello.com/b/XJuLQFXF/un20")
